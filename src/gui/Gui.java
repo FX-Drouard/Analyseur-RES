f6ff2b6667568d2384a128b1cfc847dd1beb41cd
BuildTools <unconfigured@null.spigotmc.org>
2021-11-02 10:06:15 +0100

    Ajout bouton selection de trame (methode dans Gui faite mais les appel font rien)

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -10,21 +10,31 @@ import javax.swing.JTabbedPane;
 import java.awt.BorderLayout;
 import javax.swing.JTextPane;
 import java.awt.Toolkit;
+import javax.swing.JButton;
+import java.awt.Button;
+import javax.swing.JPanel;
+import java.awt.Choice;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SpringLayout;
+import java.awt.Window.Type;
 
 public class Gui {
 
 	private JFrame frmAnalyserReseau;
 	private static Lanceur la;
+	private static int choix=0;
 
 	/**
-	 * Launch the application.
+	 * Lance La fenetre et Appel Lanceur de Input et demande avant le path du fichier a analysé.
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					String path=Question.getrep("Donnez le Path du TXT (si rien n'est donnée alors un txt de démo sera chargé)", "data/exemple2.txt");
+					//String path=Question.getrep("Donnez le Path du TXT (si rien n'est donnée alors un txt de démo sera chargé)", "data/exemple2.txt");
+					String path = "data/exemple.txt"; //en mode editeur activer ce path pour eviter le crash parser gui
 					la=new Lanceur(path);//a changer
 					Gui window = new Gui(path);
 					window.frmAnalyserReseau.setVisible(true);
@@ -36,24 +46,41 @@ public class Gui {
 	}
 
 	/**
-	 * Create the application.
+	 * Initialise et Lance la fenetre graphique.
+	 * @param path Le path du fichier
 	 */
 	public Gui(String path) {
 		initialize(path);
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Gere la fenetre , ecrit dans les differente section et change dynamiquement de trame.
+	 * @param path Le path du fichier
 	 */
 	private void initialize(String path) {
 		frmAnalyserReseau = new JFrame();
+		frmAnalyserReseau.setResizable(false);
 		frmAnalyserReseau.setTitle("Analyseur Reseau ("+path+")");
-		frmAnalyserReseau.setAlwaysOnTop(true);
-		frmAnalyserReseau.setBounds(100, 100, 607, 410);
+		frmAnalyserReseau.setBounds(100, 100, 876, 502);
 		frmAnalyserReseau.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frmAnalyserReseau.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel Trame = new JPanel();
+		tabbedPane.addTab("Choix de la Trame", null, Trame, null);
+		SpringLayout sl_Trame = new SpringLayout();
+		Trame.setLayout(sl_Trame);
+		
+		Choice choice = new Choice();
+		sl_Trame.putConstraint(SpringLayout.NORTH, choice, 207, SpringLayout.NORTH, Trame);
+		sl_Trame.putConstraint(SpringLayout.WEST, choice, 356, SpringLayout.WEST, Trame);
+		sl_Trame.putConstraint(SpringLayout.SOUTH, choice, -222, SpringLayout.SOUTH, Trame);
+		sl_Trame.putConstraint(SpringLayout.EAST, choice, -393, SpringLayout.EAST, Trame);
+		for (int i=0;i<la.nbChoix();i++) {
+			choice.add("Trame: "+(i+1));
+		}
+		Trame.add(choice);
 		
 		JTextPane Brut = new JTextPane();
 		Brut.setText(la.brutToString());
@@ -69,6 +96,23 @@ public class Gui {
 		
 		JTextPane Protocole = new JTextPane();
 		tabbedPane.addTab(la.protocolNameToString(), null, Protocole, null);
+		
+		Button bouton = new Button("Valider");
+		sl_Trame.putConstraint(SpringLayout.NORTH, bouton, 0, SpringLayout.NORTH, choice);
+		sl_Trame.putConstraint(SpringLayout.WEST, bouton, 27, SpringLayout.EAST, choice);
+		sl_Trame.putConstraint(SpringLayout.EAST, bouton, -243, SpringLayout.EAST, Trame);
+		bouton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int a=choice.getSelectedIndex();
+				la.setectData(a);
+				IP.setText(la.ipHToString());
+				Ethernet.setText(la.ethernetToString());
+				Brut.setText(la.brutToString());
+				tabbedPane.setTitleAt(4,la.protocolNameToString());
+				Question.info("Trame: "+(a+1)+" Chargée avec succes!");
+			}
+		});
+		Trame.add(bouton);
 	}
 
 }
--- src/input/Lanceur.java
+++ src/input/Lanceur.java
@@ -3,11 +3,19 @@ import java.io.IOException;
 
 public class Lanceur {
 	private final String datas;
+	private String select;
 	public Lanceur(String path) throws IOException {
 		this.datas=Input_Parser.parse(path);
+		this.setectData(0);
+	}
+	public void setectData(int choix) {
+		//a changer
+		
+		select=datas;
 	}
 	public String ethernetToString() {
 		StringBuilder res=new StringBuilder();
+		
 		String[] tmp=Input_Parser.split(datas);
 		String[] ethernet=Input_Parser.ethernetData(tmp);
 		res.append(Input_Parser.ethernetToString(ethernet));
@@ -27,5 +35,9 @@ public class Lanceur {
 		String[] tmp=Input_Parser.split(datas);
 		String[] ipH=Input_Parser.ipHData(tmp);
 		return Input_Parser.protocolenameToString(ipH);
+	}
+	public int nbChoix() {
+		//a changer
+		return 1;
 	}
 }
